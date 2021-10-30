fafa2adc07a6bc57ab2ee00c8be58f90e434c2e0
ThaiLe <levohuuthai1@gmail.com>
2021-10-31 00:10:31 +0700

    api gateway

---
+++ src/main/java/com/example/clodapigateway/ClodApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.clodapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClodApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClodApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clodapigateway/ClodApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clodapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClodApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
